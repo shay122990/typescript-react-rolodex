0580c0bda056089a7ead7eca9071b0da92bfd65c
Shay.Asanova <shay.asanova@gmail.com>
2022-05-19 22:51:30 +0400

    converted App and Cards to tsx

--- src/components/card-list/card-list.component.tsx
+++ src/components/card-list/card-list.component.tsx
@@ -1,7 +1,11 @@
-import Card from "../card/card.component"
 import "./card-list.styles.css"
+import Card from "../card/card.component"
+import {Monster} from '../../App'
 
-const CardList = ({ monsters }) => {
+type CardListProps ={
+  monsters: Monster[],
+}
+const CardList = ({ monsters }: CardListProps) => {
   return (
     <div className='card-list'>
       {monsters.map((monster) => {
--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,11 +1,12 @@
 import "./search-box.styles.css"
-import {ChangeEventHandler} from 'react'
+import {ChangeEvent} from 'react'
 
 type SearchBoxProps = {
   className: string;
   placeholder?: string;
-  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+  onChangeHandler: (event: ChangeEvent<HTMLInputElement>)=> void;
 }
+
 const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => {
   return (
     <input
