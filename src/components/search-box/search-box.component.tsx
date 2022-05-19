125027b5d25007320ba3e025f0e2fbfaf590bbfe
Shay.Asanova <shay.asanova@gmail.com>
2022-05-19 21:32:31 +0400

    converting searchbox component to tsx

---
+++ src/components/search-box/search-box.component.tsx
@@ -0,0 +1,20 @@
+import "./search-box.styles.css"
+import {ChangeEventHandler} from 'react'
+
+type SearchBoxProps = {
+  className: string;
+  placeholder?: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+}
+const SearchBox = ({ className, placeholder, onChangeHandler }: SearchBoxProps) => {
+  return (
+    <input
+      className={`search-box ${className}`}
+      type='search'
+      placeholder={placeholder}
+      onChange={ onChangeHandler}
+    />
+  )
+}
+
+export default SearchBox
